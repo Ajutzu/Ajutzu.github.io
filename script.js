dbfa28b2d491b4f5a9d21705c4a5484e461f03cf
Aeron James A. Castillo <aeronjamescastillo6@gmail.com>
2024-09-11 16:09:15 +0800

    responsive update on phone

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
-const texts = ["WELCOME TO MY PORTFOLIO", "I'M AJ CASTILLO", "EXPLORE MY PROJECTS", "PROGRAMMER"];
-let index = 0;
-const typingText = document.getElementById("typing-text");
 
-function type() {
-    typingText.textContent = '';
-    let text = texts[index];
-    let i = 0;
-    
-    function typeNext() {
-        if (i < text.length) {
-            typingText.textContent += text.charAt(i);
-            i++;
-            setTimeout(typeNext, 100); 
-        }
-    }
-    
-    typeNext();
-    
-    setTimeout(() => {
-        index = (index + 1) % texts.length;
-        setTimeout(type, 1500); 
-    }, text.length * 100 + 2000); 
-}
-
-type();
 
 function showPopup() {
     document.getElementById('popup-overlay').classList.add('popup-show');
