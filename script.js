b9c0d33b19b8c2ee64e307e07a5c77b1faf14481
Aeron James A. Castillo <aeronjamescastillo6@gmail.com>
2024-09-20 23:25:43 +0800

    new personal website design

--- script.js
+++ script.js
@@ -1,41 +1,10 @@
 
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
 
-function showPopup() {
-    document.getElementById('popup-overlay').classList.add('popup-show');
-    document.getElementById('popup-content').classList.add('popup-show');
-    return false; 
-}
-
-function hidePopup() {
-    document.getElementById('popup-overlay').classList.remove('popup-show');
-    document.getElementById('popup-content').classList.remove('popup-show');
-}
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-}
-
-function showPopup() {
-    document.getElementById('popup-overlay').classList.add('popup-show');
-    document.getElementById('popup-content').classList.add('popup-show');
-    return false; 
-}
-
-function hidePopup() {
-    document.getElementById('popup-overlay').classList.remove('popup-show');
-    document.getElementById('popup-content').classList.remove('popup-show');
-}
