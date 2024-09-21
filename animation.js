9bb3139f179d7a3d54d16862191f10fd48fd4f1c
Aeron James A. Castillo <aeronjamescastillo6@gmail.com>
2024-09-21 11:13:00 +0800

    updated

---
+++ animation.js
@@ -0,0 +1,7 @@
+// Loading Page Sections
+
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    window.location.href = "main.html";
+  }, 2000);
+});
--- script.js
+++ script.js
@@ -1,10 +1,49 @@
+// On Scroll Link to Sections
 
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
   });
+});
 
+// On Scroll Showing Sections
+
+function revealOnScroll() {
+  const sections = [
+    ".about-section",
+    ".skills-section",
+    ".education",
+    ".projects-section",
+  ];
+
+  sections.forEach((value) => {
+    const section = document.querySelector(value);
+    if (section) {
+      const sectionPosition = section.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+
+      if (sectionPosition < windowHeight) {
+        section.classList.add("show");
+      }
+    }
+  });
+}
+
+// On Hero Showing Sections
+
+function revealHero() {
+  const hero = document.querySelector(".hero");
+  if (hero) {
+    hero.classList.add("show");
+  }
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+window.addEventListener("load", () => {
+  revealHero();
+  revealOnScroll();
+});
